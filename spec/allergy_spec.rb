55973453a93cd046d982941b220e77cfcb22a601
Jesse James & Chase Reckling <student@epicodus.com>
2015-04-23 15:44:23 -0700

    write spec for multiple allergies

--- spec/allergy_spec.rb
+++ spec/allergy_spec.rb
@@ -5,4 +5,6 @@ describe('String#allergy') do
   it('takes a string with an exact allergy score and returns the corresponding allergy') do
     expect(('128').allergy()).to(eq("cats"))
   end
+  it('takes a string with a non-exact allergy score and returns the corresponding allergies') do
+    expect(('192').allergy()).to(eq("cats and pollen"))
 end
